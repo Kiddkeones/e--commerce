77ce897d61bffd80d16975c7b4f3db981b1a48fe
alejod <dedominicci.alejo@gmail.com>
2023-07-24 12:06:55 -0300

    Finalize la parte de descripcion de zapatillas y agregue JS

---
+++ index.js
@@ -0,0 +1,23 @@
+//Constantes Toggle Titles
+const toggleDescription = document.querySelector(".title-description")
+const toggleAdditionalInformation = document.querySelector(".title-additional-information")
+const toggleReviews = document.querySelector(".title-reviews")
+
+// Constantes contenido Texto
+const contentDescriprion = document.querySelector(".text-description")
+const contentAddiotionalInformation = document.querySelector(".text-additional-information")
+const contentreviews = document.querySelector(".text-reviews")
+
+//funciones toggle
+
+toggleDescription.addEventListener("click", () =>{
+    contentDescriprion.classList.toggle("hidden");
+});
+
+toggleAdditionalInformation.addEventListener("click", () =>{
+    contentAddiotionalInformation.classList.toggle("hidden");
+});
+
+toggleReviews.addEventListener("click", () =>{
+    contentreviews.classList.toggle("hidden");
+});
